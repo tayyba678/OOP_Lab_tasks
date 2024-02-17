8cf824e4e8784da6509a2f258de056b811b9eb39
Tayyba Aftab <144408026+tayyba678@users.noreply.github.com>
2024-02-17 01:23:27 -0800

    Add files via upload

---
+++ pd3Calculatortask1/pd3Calculatortask1/BL/Calcuator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pd3Calculatortask1
+{
+     class Calculator
+    {
+        public float Number1;
+        public float Number2;
+        public Calculator(float Number1, float Number2)
+        {
+            this.Number1 = Number1;
+            this.Number2 = Number2;
+        }
+        public float Add()
+        {
+            return this.Number1 + this.Number2;
+        }
+        public float Subtract()
+        {
+            return this.Number1 - this.Number2;
+        }
+        public float Multiply()
+        {
+            return this.Number1 * this.Number2;
+        }
+        public float Divide()
+        {
+            if(this.Number1 <= Number2 || this.Number2 ==0) 
+            {
+                return 0;
+            }
+            return this.Number1 / this.Number2;
+        }
+        public float Modulo()
+        {
+            return this.Number1 % this.Number2;
+        }
+    }
+}
---
+++ pd3Calculatortask1/pd3Calculatortask1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pd3Calculatortask1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 1st number: ");
+            float n1=float.Parse(Console.ReadLine());
+            Console.WriteLine("Enter 2nd number: ");
+            float n2=float.Parse(Console.ReadLine());
+            Calculator num = new Calculator(n1, n2);
+
+            Console.Clear();
+
+            int ch;
+            do
+            {
+
+                ch = Menu();
+                if (ch==1)
+                {
+                    Console.WriteLine(num.Add());
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                if (ch == 2)
+                {
+                    Console.WriteLine(num.Subtract());
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                if (ch == 3)
+                {
+                    Console.WriteLine(num.Multiply());
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                if (ch == 4)
+                {
+                    Console.WriteLine(num.Divide());
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                if (ch == 5)
+                {
+                    Console.WriteLine(num.Modulo());
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+
+            } while (ch != 6);
+
+        }
+        static int Menu()
+        {
+            Console.WriteLine("1. Add");
+            Console.WriteLine("2. Subtract");
+            Console.WriteLine("3. Multiply");
+            Console.WriteLine("4. Divide");
+            Console.WriteLine("5. Modulo");
+            Console.WriteLine("6. Exit");
+            int option = int.Parse(Console.ReadLine());
+            return option;
+        }
+    }
+}
