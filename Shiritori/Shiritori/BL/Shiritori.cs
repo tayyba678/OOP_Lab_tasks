26e56795d1961870ce412b9d8a0da844c7fad623
Tayyba Aftab <144408026+tayyba678@users.noreply.github.com>
2024-02-17 01:51:59 -0800

    Add files via upload

---
+++ Shiritori/Shiritori/BL/Shiritori.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Shiritori
+    {
+        public string[] words = new string[100];
+        public int currentIndex = 0;
+
+        public string Game(string alp)
+        {
+            if (currentIndex > 0 && words[currentIndex - 1].Last() != alp[0])
+            {
+                DisplayWords();
+                return "Game Over!";
+            }
+
+            words[currentIndex] = alp;
+            currentIndex++;
+
+            return null;
+        }
+
+        private void DisplayWords()
+        {
+            for (int i = 0; i < currentIndex; i++)
+            {
+                Console.WriteLine(words[i]);
+            }
+        }
+    }
+}
---
+++ Shiritori/Shiritori/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter words ");
+            Shiritori myShiritori = new Shiritori();
+
+            for (int i = 0; i < 100; i++)
+            {
+                string alp = Console.ReadLine();
+
+                string result = myShiritori.Game(alp);
+                if (result != null)
+                {
+                    Console.WriteLine(result);
+                    Console.ReadLine();
+                    break;
+                }
+            }
+        }
+    }
+}
